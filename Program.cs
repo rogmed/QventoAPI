a0eff13baee0fb07109ccf800d640395daa0e3bc
Rogelio Medina <rogeliomdn@gmail.com>
2022-11-07 21:57:39 +0100

    Trying to run Swagger on production

--- Program.cs
+++ Program.cs
@@ -10,12 +10,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
     
 app.UseSwagger();
+app.UseSwaggerUI();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwaggerUI();
-}
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
