9ad1da6740d7645682f40034e87103d9f77b260c
Rogelio Medina <rogeliomdn@gmail.com>
2022-11-20 21:31:42 +0100

    Add Vault to get connectionString for database securely

--- QventoAPI/Program.cs
+++ QventoAPI/Program.cs
@@ -1,7 +1,16 @@
+using Azure.Identity;
+using QventoAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Azure Vault
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+builder.Configuration.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
 
+// Local Vault
+Vault.GetConnectionString();
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
