1b486885c70f42c3623ea6769555b07f09a3b09f
Rogelio Medina <rogeliomdn@gmail.com>
2022-11-23 22:33:58 +0100

    Try basic test for GET Qvento

---
+++ TestQventoAPI/TestQventoFacade.cs
@@ -0,0 +1,28 @@
+﻿using QventoAPI;
+
+namespace TestQventoAPI
+{
+
+    [TestClass]
+    public class TestQventoFacade
+    {
+        QventoFacade facade = new QventoFacade();
+
+        [AssemblyInitialize]
+        public static void AssemblyInit(TestContext context)
+        {
+            var task = Vault.GetConnectionString();
+            task.Wait();
+        }
+
+        [TestMethod]
+        public void Get()
+        {
+            var qvento = facade.Get(100);
+
+            Assert.IsNotNull(qvento);
+
+            Assert.AreEqual(100, qvento.QventoId);
+        }
+    }
+}
