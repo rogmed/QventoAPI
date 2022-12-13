702bf1cdc5ac25c11fe9e4ea7f88463f919d7736
Rogelio Medina <rogeliomdn@gmail.com>
2022-12-13 01:55:21 +0100

    Fix Program.cs conflict

--- QventoAPI/Program.cs
+++ QventoAPI/Program.cs
@@ -8,9 +8,6 @@ using Microsoft.EntityFrameworkCore;
 // CORS
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
-// CORS
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
