50f8b98baf302e3db3c3b1ceb5621bab64967ba9
Rogelio Medina <73010972+rogmed@users.noreply.github.com>
2022-12-16 15:11:22 +0100

    Post update invitations (#37)

---
+++ QventoAPI/Controllers/InvitationController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QventoAPI.Data;
+using QventoAPI.Dto;
+using QventoAPI.Facades;
+
+namespace QventoAPI.Controllers
+{
+    /// <summary>
+    ///    Public API controller for Invitations to Qventos
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvitationController : ControllerBase
+    {
+        private InvitationFacade facade = new InvitationFacade(new Data.QventodbContext());
+
+        /// <summary>
+        ///     Post a new Invitation given a dto with QventoId and UserId
+        /// </summary>
+        /// <param name="dto">New Invitation Dto</param>
+        /// <returns>Ok(invitation)</returns>
+        [HttpPost]
+        public IActionResult Post([FromBody] NewInvitationDto dto)
+        {
+            Invitation invitation = new()
+            {
+                QventoId = dto.QventoId,
+                UserId = dto.UserId
+            };
+
+            MessageDto message = new MessageDto();
+
+            if (facade.Exists(ref invitation, ref message))
+                return Conflict(message);
+
+
+            if (!facade.Save(ref invitation))
+                return UnprocessableEntity(invitation);
+
+            return Ok(invitation);
+        }
+
+        /// <summary>
+        ///    Updates the status of an Invitation given a dto with a new status
+        /// </summary>
+        /// <param name="dto">Invitation Dto</param>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult Update([FromBody] InvitationDto dto)
+        {
+            var foundInvitation = facade.Get(dto);
+            if (foundInvitation == null)
+                return UnprocessableEntity(dto);
+
+            MessageDto message = new();
+            if (!facade.Update(ref foundInvitation, dto, ref message))
+                return UnprocessableEntity(message);
+
+            return Ok(foundInvitation);
+        }
+    }
+}
---
+++ QventoAPI/Dto/InvitationDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QventoAPI.Dto
+{
+    public class InvitationDto
+    {
+        [Required]
+        public int QventoId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        [RegularExpression("[PAR]",
+            ErrorMessage = "Status must be P (Pending), A (Aaccepted) or R (Rejected)")]
+        public string? Status { get; set; }
+    }
+
+    public class NewInvitationDto
+    {
+        [Required]
+        public int QventoId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+    }
+}
